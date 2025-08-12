7ab576bd132a78a1619f047431f92f17cb98653f
Rehan-Multani <mohammadrehan00121@gmail.com>
2025-08-12 15:36:49 +0530

    check out api

--- controllers/Admin-OrderCtrl.js
+++ controllers/Admin-OrderCtrl.js
@@ -1,4 +1,5 @@
 const Order = require('../models/Admin-OrderModel');
+const Cheackout = require('../models/CheackoutModel.js');
 
 const createOrder = async (req, res) => {
     try {
@@ -20,7 +21,29 @@ const getOrders = async (req, res) => {
     }
 }
 
+const createCheackout = async (req, res) => {
+    try {
+        const newOrder = new Cheackout(req.body);
+        const savedOrder = await newOrder.save();
+
+        res.status(201).json(savedOrder);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+const getcheckout = async (req, res) => {
+    try {
+        const orders = await Cheackout.find();
+        res.status(200).json(orders);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+}
+
 module.exports = {
     getOrders,
-    createOrder
+    createOrder,
+    getcheckout,
+    createCheackout
 }
--- routes/Admin-OrderRoute.js
+++ routes/Admin-OrderRoute.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { createOrder,getOrders } = require("../controllers/Admin-OrderCtrl");
+const { createOrder,getOrders , getcheckout , createCheackout} = require("../controllers/Admin-OrderCtrl");
 
 router.post("/admin-orders", createOrder)
 router.get("/admin-orders", getOrders)
+router.post("/checkout", getcheckout)
+router.get("/checkout", createCheackout)
 
 module.exports = router; 
